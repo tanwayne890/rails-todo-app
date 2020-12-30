52a1f7e48094c4d6037214e6936e37dcfa5ca3f2
WayneTanKinLun <waynetan2000@gmail.com>
2020-12-30 13:55:00 +0800

    Add style for tag and task
    -title stlye
    -task list in tag component

--- app/javascript/components/Tag.jsx
+++ app/javascript/components/Tag.jsx
@@ -1,55 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import CompletedTaskButton from "./buttons/CompletedTaskButton"
-import IncompleteTaskButton from "./buttons/IncompleteTaskButton"
+import CompletedTaskButton from "./buttons/CompletedTaskButton";
+import IncompleteTaskButton from "./buttons/IncompleteTaskButton";
 
 class Tag extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       tag: {},
-      tasks: []
-   };
+      tasks: [],
+    };
     this.deleteTag = this.deleteTag.bind(this);
   }
 
   componentDidMount() {
     const {
       match: {
-        params: { id }
-      }
+        params: { id },
+      },
     } = this.props;
 
     const tagUrl = `/api/v1/tag/show/${id}`;
 
-    const tasksUrl = `/api/v1/tags/${id}/tasks/index`
+    const tasksUrl = `/api/v1/tags/${id}/tasks/index`;
 
     fetch(tagUrl)
-      .then(response => {
+      .then((response) => {
         if (response.ok) {
           return response.json();
         }
         throw new Error("Network response was not ok.");
       })
-      .then(response => this.setState({ tag: response }))
+      .then((response) => this.setState({ tag: response }))
       .catch(() => this.props.history.push("/tags"));
 
     fetch(tasksUrl)
-      .then(response => {
+      .then((response) => {
         if (response.ok) {
           return response.json();
         }
         throw new Error("Network response was not ok.");
       })
-      .then(response => this.setState({ tasks: response }))
+      .then((response) => this.setState({ tasks: response }))
       .catch(() => this.props.history.push("/tags"));
   }
 
   deleteTag() {
     const {
       match: {
-        params: { id }
-      }
+        params: { id },
+      },
     } = this.props;
     const url = `/api/v1/tag/destroy/${id}`;
     const token = document.querySelector('meta[name="csrf-token"]').content;
@@ -58,17 +58,17 @@ class Tag extends React.Component {
       method: "DELETE",
       headers: {
         "X-CSRF-Token": token,
-        "Content-Type": "application/json"
-      }
+        "Content-Type": "application/json",
+      },
     })
-      .then(response => {
+      .then((response) => {
         if (response.ok) {
           return response.json();
         }
         throw new Error("Network response was not ok.");
       })
       .then(() => this.props.history.push("/tags"))
-      .catch(error => console.log(error.message));
+      .catch((error) => console.log(error.message));
   }
 
   render() {
@@ -76,60 +76,76 @@ class Tag extends React.Component {
     let taskList = "No task available";
 
     if (tasks.length > 0) {
-      taskList = tasks
-        .map((task, index) => (
-          <li key={index} className="list-group-item">
-            <div className="card mb-4">
-              <div className="card-body">
-                <h5 className="card-title">{task.description}</h5>
-                {task.completed ? <CompletedTaskButton id={task.id} tag_id={tag.id}/> 
-                : <IncompleteTaskButton id={task.id} tag_id={tag.id}/>}
-                <Link to={`/tag/${tag.id}/task/${task.id}`} className="btn custom-button">
-                  View task
-                </Link>
-              </div>
+      taskList = tasks.map((task, index) => (
+        <li key={index} className="list-group-item">
+          <div className="d-flex">
+            <div className="p-2">
+              <h5>{task.description}</h5>
             </div>
-          </li>
-          // completebutton, delete button
-        ));
-    }
-    
-    return (
-      <div className="">
-        <div className="hero position-relative d-flex align-items-center justify-content-center">
-          <div className="overlay bg-dark position-absolute" />
-          <h1 className="display-4 position-relative text-white">
-            {tag.title}
-          </h1>
-          
-        </div>
-        <div className="container py-5">
-          <div className="row">
-            <div className="col-sm-12 col-lg-3">
-              <ul className="list-group">
-                <h5 className="mb-2">Task list</h5>
-                <Link to={`/new_task/${tag.id}`} className="btn btn-link">
-                Create new task
-                </Link>
-                {taskList}
-              </ul>
+            <div className="p-2">
+              {task.completed ? (
+                <CompletedTaskButton id={task.id} tag_id={tag.id} />
+              ) : (
+                <IncompleteTaskButton id={task.id} tag_id={tag.id} />
+              )}
             </div>
-        
-            <div className="col-sm-12 col-lg-2">
-              <button type="button" className="btn btn-danger" onClick={this.deleteTag}>
-                Delete Tag
-              </button>
+            <div className="ml-auto p-2">
+              <Link
+                to={`/tag/${tag.id}/task/${task.id}`}
+                className="btn custom-button"
+              >
+                View task
+              </Link>
             </div>
           </div>
-          <Link to="/tags" className="btn btn-link">
-            Back to tags
-          </Link>
+        </li>
+      ));
+    }
+
+    return (
+      <div>
+        <section className="jumbotron jumbotron-fluid text-center">
+          <div className="container py-5">
+            <h1 className="display-4">{tag.title}</h1>
+          </div>
+        </section>
+
+        <div className="container py-5">
+          <div className="row justify-content-md-center">
+            <div className="col col-lg-2"></div>
+            <div className="col-md-10">
+              <div className="column">
+                <div className="mb-2">
+                  <h5>Task list</h5>
+                  <Link to={`/new_task/${tag.id}`} className="btn btn-link">
+                    Create new task
+                  </Link>
+                </div>
+
+                <div className="col-sm-12 col-lg-10">
+                  <ul className="list-group">{taskList}</ul>
+                </div>
+
+                <div className="col-sm-12 col-lg-10">
+                  <button
+                    type="button"
+                    className="btn btn-danger"
+                    onClick={this.deleteTag}
+                  >
+                    Delete Tag
+                  </button>
+                </div>
+              </div>
+              <Link to="/tags" className="btn btn-link">
+                Back to tags
+              </Link>
+            </div>
+            <div className="col col-lg-2"></div>
+          </div>
         </div>
       </div>
     );
   }
-
-
 }
 
 export default Tag;
--- app/javascript/components/Task.jsx
+++ app/javascript/components/Task.jsx
@@ -4,37 +4,37 @@ import { Link } from "react-router-dom";
 class Task extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      task: {}
-   };
+    this.state = {
+      task: {},
+    };
     this.deleteTask = this.deleteTask.bind(this);
   }
 
   componentDidMount() {
     const {
       match: {
-        params: { id, tag_id }
-      }
+        params: { id, tag_id },
+      },
     } = this.props;
 
-    const taskUrl = `/api/v1/tags/${tag_id}/task/show/${id}`
+    const taskUrl = `/api/v1/tags/${tag_id}/task/show/${id}`;
 
     fetch(taskUrl)
-      .then(response => {
+      .then((response) => {
         if (response.ok) {
           return response.json();
         }
         throw new Error("Network response was not ok.");
       })
-      .then(response => this.setState({ task: response }))
+      .then((response) => this.setState({ task: response }))
       .catch(() => this.props.history.push(`/tag/${tag_id}`));
   }
 
   deleteTask() {
     const {
       match: {
-        params: { id, tag_id }
-      }
+        params: { id, tag_id },
+      },
     } = this.props;
     const url = `/api/v1/tags/${tag_id}/task/destroy/${id}`;
     const token = document.querySelector('meta[name="csrf-token"]').content;
@@ -43,37 +43,40 @@ class Task extends React.Component {
       method: "DELETE",
       headers: {
         "X-CSRF-Token": token,
-        "Content-Type": "application/json"
-      }
+        "Content-Type": "application/json",
+      },
     })
-      .then(response => {
+      .then((response) => {
         if (response.ok) {
           return response.json();
         }
         throw new Error("Network response was not ok.");
       })
       .then(() => this.props.history.push(`/tag/${tag_id}`))
-      .catch(error => console.log(error.message));
+      .catch((error) => console.log(error.message));
   }
 
   render() {
     const { task } = this.state;
-    
+
     return (
-      <div className="">
-        <div className="hero position-relative d-flex align-items-center justify-content-center">
-          <div className="overlay bg-dark position-absolute" />
-          <h1 className="display-4 position-relative text-white">
-            {task.description}
-          </h1>
-          
-        </div>
-        <div className="container py-5">   
-            <div className="col-sm-12 col-lg-2">
-              <button type="button" className="btn btn-danger" onClick={this.deleteTask}>
-                Delete Task
-              </button>
-            </div>  
+      <div>
+        <section className="jumbotron jumbotron-fluid text-center">
+          <div className="container py-5">
+            <h1 className="display-4">{task.description}</h1>
+          </div>
+        </section>
+
+        <div className="container py-5">
+          <div className="col-sm-12 col-lg-2">
+            <button
+              type="button"
+              className="btn btn-danger"
+              onClick={this.deleteTask}
+            >
+              Delete Task
+            </button>
+          </div>
           <Link to={`/tag/${task.tag_id}`} className="btn btn-link">
             Back to tag
           </Link>
@@ -81,8 +84,6 @@ class Task extends React.Component {
       </div>
     );
   }
-
-
 }
 
 export default Task;
